766828539c6883dce13309985124ea85baa16cd8
martin.carrera <martin.carrera@tabbedout.com>
2016-03-04 14:36:59 -0300

    Finished Level 5

--- GoodAsOldPhones/MCProductsTableViewController.swift
+++ GoodAsOldPhones/MCProductsTableViewController.swift
@@ -9,16 +9,42 @@
 import UIKit
 
 class MCProductsTableViewController: UITableViewController {
+    
+    var productNames: [String]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productNames = ["1907 Wall Set", "1921 Dial Phone", "1937 Desk Set", "1984 Motorola Portable"]
+    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if let pNames = productNames {
+            return pNames.count
+        }
+        return 0
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell", forIndexPath: indexPath)
-        cell.textLabel?.text = "Hello Friend"
+
+        let productName = productNames?[indexPath.row]
+        if let pName = productName {
+            cell.textLabel?.text = pName
+        }
+        
         cell.imageView?.image = UIImage(named: "image-cell1")
         return cell
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "ShowProduct" {
+            let productVC = segue.destinationViewController as? MCProductViewController
+            
+            guard let cell = sender as? UITableViewCell,
+                let indexPath = tableView.indexPathForCell(cell) else {
+                    return
+            }
+            productVC?.productName = productNames?[indexPath.row]
+        }
+    }
 }
